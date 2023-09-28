2303e7bbff449221f6498dfaa5ea139bb8952198
hanshublyh <2539617403@stu.xjtu.edu.cn>
2023-09-28 20:50:07 +0800

    first commit

---
+++ readme.txt
@@ -0,0 +1,8 @@
+1.分别运行run_a1.sh, run_a1_1.sh, run_a3.sh a1测试两种参数，a3只测试一种，虽然文件夹给出了a3的两种参数，但只测一种
+2.main.ipynb是生成数据用的
+3.deivce是所有文件共用的参数 指定gpu
+4.5_40不运行 是我自己测试并且生成数据用的 
+5.每种文件之后的para1.txt para3.txt是单和多智能体的参数 从80_200-->150_500基本一致
+6.在para1.txt para3.txt para1_1.txt里 batch_size统一设置为512
+7.para1和para3的参数基本一致
+8.使用resnet预训练参数的大规模的数据集 input是224 * 224 / 448 * 448
---
+++ run_a1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+a="80_200"
+b="100_300"
+c="120_400"
+d="150_500"
+exp="NoExp"
+r="r3"
+l="r1+r2"
+for file in a b c d;do
+    cd $file
+    for i in 1 2 3; do
+        cd $i
+        cd $exp
+        cd $r
+        python a1.py >> a1.txt
+        cd ..
+        cd $l 
+        python a1.py >> a1.txt
+        cd ../../../
+    done
+    cd ..
+done
